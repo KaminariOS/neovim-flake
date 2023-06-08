90e3328a054fa7185c32c846c456d1cbb289b98d
kosumi <chankocyo@gmail.com>
2023-06-08 02:38:27 -0600

    Better path for rust debug

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -153,7 +153,7 @@ in {
             type = 'lldb',
             request = 'launch',
             program = function()
-              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug', 'file')
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
             end,
             cwd = "''${workspaceFolder}",
             stopOnEntry = false,
