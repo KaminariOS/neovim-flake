18211661f19584c6f2f2032ac15f2e19d49045d5
kosumi <chankocyo@gmail.com>
2024-01-22 11:23:55 -0700

    Add vscode html server

--- modules/languages/html.nix
+++ modules/languages/html.nix
@@ -28,11 +28,31 @@ in {
   };
 
   config = mkIf cfg.enable (mkMerge [
+    (mkIf cfg.enable {
+      vim.startPlugins = with pkgs; [ vscode-langservers-extracted ];
+      vim.luaConfigRC.html = ''
+        --Enable (broadcasting) snippet capability for completion
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+        require'lspconfig'.html.setup{
+          capabilities = capabilities,
+          cmd = { "${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server", "--stdio" }
+        }
+        require'lspconfig'.cssls.setup{
+          capabilities = capabilities,
+          cmd = { "${pkgs.vscode-langservers-extracted}/bin/vscode-css-language-server", "--stdio" }
+        }
+        require'lspconfig'.eslint.setup{
+          cmd = { "${pkgs.vscode-langservers-extracted}/bin/vscode-eslint-language-server", "--stdio" }
+        }
+      '';
+    })
     (mkIf cfg.treesitter.enable {
       vim.treesitter.enable = true;
       vim.treesitter.grammars = [cfg.treesitter.package];
 
-      vim.startPlugins = optional cfg.treesitter.autotagHtml "nvim-ts-autotag";
+      vim.startPlugins = (optional cfg.treesitter.autotagHtml "nvim-ts-autotag")
+      ;
 
       vim.luaConfigRC.html-autotag = mkIf cfg.treesitter.autotagHtml (nvim.dag.entryAnywhere ''
         require('nvim-ts-autotag').setup()
