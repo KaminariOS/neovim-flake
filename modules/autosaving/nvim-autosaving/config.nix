d27351997b6d27abc35e6fae442731d757bbae95
kosumi <chankocyo@gmail.com>
2024-09-16 13:10:37 -0400

    Fix autosave

--- modules/autosaving/nvim-autosaving/config.nix
+++ modules/autosaving/nvim-autosaving/config.nix
@@ -12,5 +12,11 @@ in {
     mkIf cfg.enable
     {
       vim.startPlugins = ["auto-save-nvim"];
+      vim.luaConfigRC.autosave = nvim.dag.entryAnywhere ''
+          require("auto-save").setup {
+		-- your config goes here
+		-- or just leave it empty :)
+	      }
+      '';
     };
 }
