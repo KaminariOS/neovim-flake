c3aee88da8e3be54beb05b7c821534722450f9e4
kosumi <chankocyo@gmail.com>
2025-05-26 19:57:53 -0400

    Add custom keybindings

--- init.lua
+++ init.lua
@@ -145,3 +145,6 @@ vim.api.nvim_create_autocmd({ "TextChanged"}, {
 })
 
 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(), { bufnr })
+
+vim.keymap.set("n", "<leader>ca", ":lua require('crates').show_popup()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true })
